f02523530db48ab179e7456fd908e6d30e6e5ab4
Mary <mary_hbb@yahoo.com>
2023-03-14 00:21:31 +0100

    implement computeTemp method with if else

--- index.js
+++ index.js
@@ -3,23 +3,40 @@ const fahrenheitEl = document.getElementById("fahrenheit");
 const kelvinEl = document.getElementById("kelvin");
 
 
+// function computeTemp(event) {
+//     const currentValue = +event.target.value;
+  
+//     switch (event.target.name) {
+//       case "celsius":
+//         kelvinEl.value = (currentValue + 273.32).toFixed(2);
+//         fahrenheitEl.value = (currentValue * 1.8 + 32).toFixed(2);
+//         break;
+//       case "fahrenheit":
+//         celsiusEl.value = ((currentValue - 32) / 1.8).toFixed(2);
+//         kelvinEl.value = ((currentValue - 32) / 1.8 + 273.32).toFixed(2);
+//         break;
+//       case "kelvin":
+//         celsiusEl.value = (currentValue - 273.32).toFixed(2);
+//         fahrenheitEl.value = ((currentValue - 273.32) * 1.8 + 32).toFixed(2);
+//         break;
+//       default:
+//         break;
+//     }
+//   }
+
 function computeTemp(event) {
     const currentValue = +event.target.value;
+    const currentUnit = event.target.name;
   
-    switch (event.target.name) {
-      case "celsius":
-        kelvinEl.value = (currentValue + 273.32).toFixed(2);
-        fahrenheitEl.value = (currentValue * 1.8 + 32).toFixed(2);
-        break;
-      case "fahrenheit":
-        celsiusEl.value = ((currentValue - 32) / 1.8).toFixed(2);
-        kelvinEl.value = ((currentValue - 32) / 1.8 + 273.32).toFixed(2);
-        break;
-      case "kelvin":
-        celsiusEl.value = (currentValue - 273.32).toFixed(2);
-        fahrenheitEl.value = ((currentValue - 273.32) * 1.8 + 32).toFixed(2);
-        break;
-      default:
-        break;
+    if (currentUnit === "celsius") {
+      kelvinEl.value = (currentValue + 273.32).toFixed(2);
+      fahrenheitEl.value = (currentValue * 1.8 + 32).toFixed(2);
+    } else if (currentUnit === "fahrenheit") {
+      celsiusEl.value = ((currentValue - 32) / 1.8).toFixed(2);
+      kelvinEl.value = ((currentValue - 32) / 1.8 + 273.32).toFixed(2);
+    } else if (currentUnit === "kelvin") {
+      celsiusEl.value = (currentValue - 273.32).toFixed(2);
+      fahrenheitEl.value = ((currentValue - 273.32) * 1.8 + 32).toFixed(2);
     }
   }
+  
