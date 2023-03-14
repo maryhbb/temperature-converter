7664b5b877eda45067f299e981919d244a0583ce
Mary <mary_hbb@yahoo.com>
2023-03-14 01:05:07 +0100

    Import style in js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import css from "./style.css";
+
+
 const celsiusEl = document.getElementById("celsius");
 const fahrenheitEl = document.getElementById("fahrenheit");
 const kelvinEl = document.getElementById("kelvin");
